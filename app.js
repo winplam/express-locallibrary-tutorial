f2b8218cf635b3576efd85a744584469877793d8
Win Lam <kaito@computer.local>
2020-05-23 13:32:42 -0700

    Update mongoDB URI

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ var app = express();
 
 // Set up mongoose connection
 var mongoose = require('mongoose');
-var mongoDB = 'mongodb+srv://expresslibrary:2PP0DFm5QF3jNgEV@cluster0-abnvt.mongodb.net/expresslibrary?retryWrites=true&w=majority';
+var mongoDB = process.env.MONGODB_URI;
 mongoose.connect(mongoDB, {useNewUrlParser: true});
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
